97f9c8fe6f9df900f4664b4376a6a3e9dd4f1730
naizal Edy <pritiadhikari@gmail.com>
2018-06-26 04:49:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function happyHolidays()
 function happyHolidaysTo(name)
 {
  
-  return `Happy holidays, ${name}!`
+  return `Happy ${holiday}, ${name}!`
 }
 
 function happyHolidayTo(holiday, name)
